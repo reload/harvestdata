127aba7f3854a610afadb4d8290d47ea6765c65c
Thomas Fini Hansen <xen@xen.dk>
2016-05-02 09:22:48 +0200

    Generate all files on first run

--- docker/etc/my_init.d/harvestdata.sh
+++ docker/etc/my_init.d/harvestdata.sh
@@ -4,3 +4,10 @@ sed -i -e "s/\$account/$HARVESTDATA_ACCOUNT/" \
     -e "s/\$username/$HARVESTDATA_USERNAME/" \
     -e "s/\$password/$HARVESTDATA_PASSWORD/" \
     /harvestdata/config.yml
+
+if [ ! -e /harvestdata/data/today.xml ]
+then
+    echo "First run, running all cron entries"
+    grep "^#" -v /harvestdata/docker/crontab | while read -r m h dom mon dow cmd; do sh -c "$cmd"; done
+    echo "Done"
+fi
